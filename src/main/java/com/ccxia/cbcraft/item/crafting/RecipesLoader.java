ddbb98e7bb3425e870a854e13f8f1eb670bbe29c
unknown <879139909@qq.com>
2020-12-09 23:43:56 +0800

    1.1.0-pre1

--- src/main/java/com/ccxia/cbcraft/item/crafting/RecipesLoader.java
+++ src/main/java/com/ccxia/cbcraft/item/crafting/RecipesLoader.java
@@ -9,16 +9,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = CbCraft.MODID)
 public class RecipesLoader {
-	@SubscribeEvent
-	public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
-		event.getRegistry().registerAll(new RecipesHotChocolate(), new RecipesCake(), new RecipesCocoa(),
-				new RecipesBread(), new RecipesInjectedIronSword(), new RecipesInjectedIronAxe(),
-				new RecipesInjectedIronPickaxe(), new RecipesInjectedIronShovel(), new RecipesInjectedIronHoe(),
-				new RecipesInjectedIronLeggings(), new RecipesInjectedIronChestPlate(), new RecipesInjectedIronHelmet(),
-				new RecipesInjectedIronBoots(), new RecipesInjectedGoldSword(), new RecipesInjectedGoldPickaxe(),
-				new RecipesInjectedGoldAxe(), new RecipesInjectedGoldShovel(), new RecipesInjectedGoldHoe(),
-				new RecipesInjectedGoldHelmet(), new RecipesInjectedGoldChestPlate(), new RecipesInjectedGoldLeggings(),
-				new RecipesInjectedGoldBoots());
-	}
+    @SubscribeEvent
+    public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+        event.getRegistry().registerAll(new RecipesCake(), new RecipesCocoa(),
+                new RecipesBread(), new RecipesInjectedIronSword(), new RecipesInjectedIronAxe(),
+                new RecipesInjectedIronPickaxe(), new RecipesInjectedIronShovel(), new RecipesInjectedIronHoe(),
+                new RecipesInjectedIronLeggings(), new RecipesInjectedIronChestPlate(), new RecipesInjectedIronHelmet(),
+                new RecipesInjectedIronBoots(), new RecipesInjectedGoldSword(), new RecipesInjectedGoldPickaxe(),
+                new RecipesInjectedGoldAxe(), new RecipesInjectedGoldShovel(), new RecipesInjectedGoldHoe(),
+                new RecipesInjectedGoldHelmet(), new RecipesInjectedGoldChestPlate(), new RecipesInjectedGoldLeggings(),
+                new RecipesInjectedGoldBoots());
+    }
 
 }
